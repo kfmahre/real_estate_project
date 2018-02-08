00c7c2c717ba13863e645e2102237cbdc37f7486
kfmahre <kfmahre@gmail.com>
2018-02-08 02:30:45 -0500

    added pictures

--- js/app.js
+++ js/app.js
@@ -39,9 +39,9 @@
       rooms: 4,
       canPurchase: true,
       images: [
-        "http://clipgoo.com/daut/as/f/h/house-interior-cost-to-paint-a-queensland-charming-futuristic-island-home-by-nigel-gee_futuristic-house-exterior_exterior_exterior-siding-design-small-house-home-ideas-landscaping-designs-paint-tool-s.jpg",
-        "http://www.builderhouseplans.com/house-plans/media/catalog/product/cache/11/image/628x353/9df78eab33525d08d6e5fb8d27136e95/a/m/ama879-fr-re-co.jpg",
-        "http://www.eplans.com/house-plans/media/catalog/product/a/m/ama879-fr-re-co.jpg"
+        "/images/house1-1.jpg",
+        "/images/house1-2.jpg",
+        "/images/house1-3.jpg"
       ],
       reviews: [{
         stars: 5,
@@ -65,8 +65,8 @@
       rooms: 3,
       canPurchase: true,
       images: [
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTy8RObu6BpVxTv89TVAgwO_hDD9YD4IYU3wwfWx4V6-GjiU24EZA",
-        "http://sunflaresolar.com/wp-content/uploads/2016/09/Solarshingles.jpg",
+        "/images/house2-1.jpg",
+        "/images/house2-2.jpg",
       ],
       reviews: [{
         stars: 3,
@@ -90,9 +90,9 @@
       rooms: 4,
       canPurchase: true,
       images: [
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRauKKX-jtPYc6L53jb11TZ0qE-FDjRpYtVCic_UZN3FKgjjcEV",
-        "https://nhadepvietnam.net/wp-content/uploads/2017/11/nhung-dieu-can-tranh-khi-chon-mau-son-ngoai-that-2-320x200.jpg",
-        "http://zootecnews.com/img/exterior-house-colors/_fullsize/prissy-exterior-color-schemes-along-with-painted-brick-exterior-color-schemes-also-ranch-style-homes-home-design-ideas-plus-ranch-style-homes-home-design-ideas-housecolors-as-wells-as-gallery_exterior-house-colors.jpg"
+        "/images/house3-1.jpg",
+        "/images/house3-2.jpg",
+        "/images/house3-3.jpg"
       ],
       reviews: [{
         stars: 1,
